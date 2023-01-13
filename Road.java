baf8fd3756f702eeac879dc291a214751f752963
unknown <basavaraj123xworkz@gmail.com>
2023-01-13 23:44:45 +0530

    he

---
+++ Road.java
@@ -0,0 +1,25 @@
+class Road{
+	static void traffic()
+	{
+		System.out.println("in bengalur lot of traffic");
+	}
+	static void footpath()
+	{
+		System.out.println("in footpath always beside the road");
+	}
+	static void divider()
+	{
+		System.out.println("in road have center divider is thear ");
+	}
+	static void median()
+	{
+		System.out.println("road have a median");
+	}
+	static void roadbrake()
+	{
+		System.out.println("road have roadbrake because to avoid a accident");
+	
+	}
+	
+	
+}
