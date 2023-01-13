c38c32fef357e5818a4c1954cb9b7607567cb06a
unknown <basavaraj123xworkz@gmail.com>
2023-01-13 22:48:31 +0530

    hi

---
+++ Building.java
@@ -0,0 +1,23 @@
+class Building{
+	static rooms()
+	{
+		System.out.println("building have number of rooms");
+	}
+	static wall()
+	{
+		System.out.println("wall are proveded to suppret the room");
+	}
+	static coloumn()
+	{
+		System.out.println("it support wall canstruction");
+	}
+	static slab()
+	{
+		System.out.println("to act as heat and fire insulator");
+	}
+	static beam()
+	{
+		System.out.println(" it balance the load of the building");
+	}
+	
+}
