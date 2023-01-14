ede3c09e643f46cbe6a5b47217b83c6f016f31b1
unknown <basavaraj123xworkz@gmail.com>
2023-01-14 09:51:44 +0530

    hello

---
+++ Sweet .java
@@ -0,0 +1,14 @@
+class Sweet{
+	    
+	static String name=("peda");
+   static double price=(200d);
+   static String[]colour={"red" , "white"};
+   static String[]ingredients={"sugar", "milk"};
+   static String shape=("round");
+   
+   
+   		
+}
+
+
+   
