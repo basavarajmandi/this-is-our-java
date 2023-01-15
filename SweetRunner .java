590aae06682c0b9e9306a678e9645b94c68dadb2
unknown <basavaraj123xworkz@gmail.com>
2023-01-15 07:51:53 +0530

    bye bye

--- SweetRunner .java
+++ SweetRunner .java
@@ -1,8 +1,11 @@
 class SweetRunner{
 	public static void main(String[] args){
-		
+	
+	System.out.println(Sweet.name);
+	Sweet.name="laddu";
 	System.out.println(Sweet.name);
 	System.out.println(Sweet.price);
+	Sweet.price="3000";
  System.out.println("ther are verious type of colour");
  
  for(int i=0;i<Sweet.colour.length;i++)
