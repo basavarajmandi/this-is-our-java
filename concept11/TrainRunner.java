50f4c722ccdbc96c894dbde93c42b0939f77d474
unknown <basavaraj123xworkz@gmail.com>
2023-01-15 23:45:43 +0530

    hii

---
+++ concept11/TrainRunner.java
@@ -0,0 +1,13 @@
+class TrainRunner{
+	public static void main(String[] args){
+		 
+	  Train.transport();
+	  Road.traffic();
+	  Train.engin();
+	  Train.wheel();
+	  Train.watertank();
+	  Train.lights();
+	  
+		
+	}
+}																											
