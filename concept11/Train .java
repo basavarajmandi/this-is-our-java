3e2a28b9080b163942a5d0df612bb08326fa6102
unknown <basavaraj123xworkz@gmail.com>
2023-01-15 22:10:26 +0530

    new coin

---
+++ concept11/Train .java
@@ -0,0 +1,26 @@
+class Train{
+
+static void transport()
+{
+	System.out.println("books transport by train");
+   transport();
+}
+static void engin()
+{
+	System.out.println("train have a good engin");
+}
+static void wheel()
+{
+	System.out.println("train have number of wheel");
+}
+static void watertank()
+{
+	System.out.println("train have qulity watertank");
+	
+}
+static void lights()
+{
+	System.out.println("inside the train number of lights");
+}
+
+}
